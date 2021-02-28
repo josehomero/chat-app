eb1636306c78840d0dc1f615f026ffffbc164070
josehomero <jose.homero.rios@outlook.com>
2021-02-28 13:36:40 -0800

    working on getting bootstrap to work

--- src/App.js
+++ src/App.js
@@ -2,8 +2,23 @@ import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div>
+      <h1>Chat App</h1>
+      <div >
+    <div>Header</div>
+    <div >
+        <p>Body</p>
+    </div>
+    <div >Footer</div>
+</div>
+
+      <input
+                    className='form-control'
+                    name="search"
+                    type="text"
+                    placeholder="Submit?"
+                     />
+                <button type="button" class="btn btn-primary">Primary</button>
     </div>
   );
 }
