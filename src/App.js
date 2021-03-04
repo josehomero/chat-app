8f278dc38bbcde7eb997d3c59fcb3ba20ac9c6a5
josehomero <jose.homero.rios@outlook.com>
2021-03-03 21:32:02 -0800

    working on the rows and columns with bootstrap

--- src/App.js
+++ src/App.js
@@ -2,23 +2,22 @@ import React from "react";
 
 function App() {
   return (
-    <div>
+    <div className="container container-fluid">
       <h1>Chat App</h1>
-      <div >
-    <div>Header</div>
-    <div >
-        <p>Body</p>
-    </div>
-    <div >Footer</div>
-</div>
+      
+      <div className="row">
+
+      </div>
 
       <input
-                    className='form-control'
-                    name="search"
-                    type="text"
-                    placeholder="Submit?"
-                     />
-                <button type="button" class="btn btn-primary">Primary</button>
+        className="form-control"
+        name="search"
+        type="text"
+        placeholder="Submit?"
+      />
+      <button type="button" class="btn btn-primary">
+        Primary
+      </button>
     </div>
   );
 }
